a0811c22cd3991cb98fc2e8cb5e8991422535a0f
mmiiles <milesconrad@pm.me>
2021-03-19 12:00:13 -0400

    initial commit

---
+++ client.py
@@ -0,0 +1,44 @@
+from pickle import loads
+import socket
+
+from pynput.mouse import Button, Controller
+mouse = Controller()
+from pynput.keyboard import Key, Controller
+keyboard = Controller()
+from pynput.keyboard._win32 import KeyCode
+
+downKeys = []
+downButtons = []
+client = socket.socket()
+client.connect(('67.149.113.14', 600))
+print('Connected!')
+
+while True:
+    client.send('confirmed'.encode())
+    data = client.recv(1024)
+    data = loads(data)
+    
+    if isinstance(data, list):
+        x, y = data
+        mouse.position = (x, y)
+
+    elif isinstance(data, Button):
+        if data not in downButtons:
+            downButtons.append(data)
+            keyboard.press(data)
+        else:
+            keyboard.release(data)
+            downButtons.remove(data)
+            
+    elif isinstance(data, int):
+        mouse.scroll(0, data)
+    
+    elif isinstance(data, Key) or isinstance(data, KeyCode):
+        if data not in downKeys:
+            downKeys.append(data)
+            keyboard.press(data)
+        else:
+            keyboard.release(data)
+            downKeys.remove(data)
+
+client.close()
---
+++ server.py
@@ -0,0 +1,51 @@
+from pickle import dumps
+import socket
+from pynput import mouse
+from pynput import keyboard
+
+server = socket.socket()
+server.bind(('192.168.0.123', 600))
+server.listen(2)
+print('Listening...')
+conn, address = server.accept()
+print('New connection from ' + address[0])
+
+def on_move(x, y):
+    conn.recv(1024)
+    position = dumps([x, y])
+    conn.send(position)
+    mouse.Controller().position = (0, 0 )
+
+def on_click(x, y, button, pressed):
+    conn.recv(1024)
+    button = dumps(button)
+    conn.send(button)
+
+def on_scroll(x, y, dx, dy):
+    conn.recv(1024)
+    direction = dumps(dy)
+    conn.send(direction)
+
+def on_press(key):
+    conn.recv(1024)
+    key = dumps(key)
+    conn.send(key)
+
+def on_release(key):
+    conn.recv(1024)
+    key = dumps(key)
+    conn.send(key)
+
+mouseListener = mouse.Listener(
+    on_move=on_move,
+    on_click=on_click,
+    on_scroll=on_scroll)
+keyboardListener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+mouseListener.start()
+keyboardListener.start()
+
+mouseListener.join()
+keyboardListener.join()
+conn.close()
